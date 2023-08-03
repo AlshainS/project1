656de69187bec8da21adaf112043d9de39fb3457
Lexeor <lexeor.ross@gmail.com>
2023-08-03 19:27:08 +0400

    add bot context interface

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,32 @@
-import { Telegraf } from "telegraf";
-import { message } from "telegraf/filters";
+import { Telegraf, session } from "telegraf";
+import { IConfigService } from "./config/config.interface";
+import { ConfigService } from "./config/config.service";
+import { IBotContext } from "./config/context.inteface";
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.launch();
+class Bot {
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+  bot: Telegraf<IBotContext>;
+
+  constructor(private readonly configService: IConfigService) {
+    this.bot = new Telegraf<IBotContext>(this.configService.get('BOT_TOKEN'));
+    this.bot.use(session());
+  }
+
+  init() {
+    this.bot.launch();
+  }
+}
+
+const bot = new Bot(new ConfigService());
+bot.init();
+
+// const bot = new Telegraf(process.env.BOT_TOKEN);
+// bot.start((ctx) => ctx.reply('Welcome'));
+// bot.help((ctx) => ctx.reply('Send me a sticker'));
+// bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+// bot.launch();
+
+// // Enable graceful stop
+// process.once('SIGINT', () => bot.stop('SIGINT'));
+// process.once('SIGTERM', () => bot.stop('SIGTERM'));
---
+++ src/config/context.inteface.ts
@@ -0,0 +1,9 @@
+import { Context } from 'telegraf';
+
+export interface SessionData {
+  boolean: boolean;
+}
+
+export interface IBotContext extends Context {
+  session: SessionData;
+}
