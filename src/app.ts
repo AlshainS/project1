6f4c94230bef86c6f5baa26e18916791804c94fe
Lexeor <lexeor.ross@gmail.com>
2023-08-08 19:47:16 +0400

    add scheduler (node-cron)

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import { TelegramBot } from "./types";
+import { Participant, TelegramBot } from "./types";
 import { IConfigService } from "./config/ConfigService.interface";
 import { ConfigService } from "./config/ConfigService";
 import { Command } from "./commands/Command";
 import { StartCommand } from "./commands/StartCommand";
 import LocalSession from "telegraf-session-local";
 import { connectToDatabase } from "./mongodb/db";
+import cron from 'node-cron';
 
 class Bot {
   bot: TelegramBot;
@@ -25,12 +26,26 @@ class Bot {
       command.handle();
     }
     this.bot.launch();
+    
+    // Scheduler (everyday at 19:00)
+    cron.schedule("0 0 19 * * *", async () => {
+      // Check participants
+      if(this.bot.db) {
+        const participansId = new Set();
+        const collection = this.bot.db.collection<Participant>("participants");
+        const cursor = collection.find();
+  
+        for await (const doc of cursor) {
+          participansId.add(doc.tg_id);
+        }
+  
+        console.log(participansId);
+      }
+      // Send reminders to them
+      //this.bot.telegram.sendMessage(214955237, "Отправлено по расписанию");
+    });
   }
 }
 
 const bot = new Bot(new ConfigService());
 bot.init();
-
-// // Enable graceful stop
-// process.once('SIGINT', () => bot.stop('SIGINT'));
-// process.once('SIGTERM', () => bot.stop('SIGTERM'));
